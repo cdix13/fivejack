f8c63aacb1a08e9100ca050d59dadc22097202af
cdix1313 <sidik.saepudin13@gmail.com>
2020-02-08 00:39:56 +0700

    question 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
 const App: React.FC = () => {
-  const [input1, setInput1] = useState([
+  const input1 = [
     'Enter uid1234 Muzi',
     'Enter uid4567 Prodo',
     'Leave uid1234',
     'Enter uid1234 Prodo',
     'Change uid4567 Ryan',
-  ]);
+  ];
+  const input3 = [
+    ['100', 'ryan', 'music', '2'],
+    ['200', 'apeach', 'math', '2'],
+    ['300', 'tube', 'computer', '3'],
+    ['400', 'con', 'computer', '4'],
+    ['500', 'muzi', 'music', '3'],
+    ['600', 'apeach', 'music', '2'],
+  ];
   const [answer1, setAnswer1] = useState();
+  const [answer3, setAnswer3] = useState();
 
   const solution1 = (record: any[] = []) => {
     const ENTER = 'Enter';
@@ -47,6 +56,55 @@ const App: React.FC = () => {
     return answer;
   };
 
+  const solution3 = (relation: any[] = []) => {
+    const cols = relation[0].length;
+    let curr = 0;
+    let max = 1;
+    let temp2: any[] = [];
+    let answer = 0;
+    while (curr < cols) {
+      let temp = [];
+      for (let idx in relation) {
+        temp.push(relation[idx][curr]);
+      }
+
+      if (new Set(temp).size === relation.length) {
+        answer += 1;
+      } else if (max <= 2) {
+        if (temp2.length > 0) {
+          // temp.push([temp2[idx], relation[idx][curr]]);
+          let combine = temp.map((t, idx) => {
+            return [temp2[idx], t];
+          });
+          let dataUnique = combine.reduce(function(out, item) {
+            return out.concat(
+              out.filter(function(comp) {
+                return item.toString() == comp.toString();
+              }).length
+                ? []
+                : [item],
+            );
+          }, []);
+
+          if (dataUnique.length === relation.length) {
+            answer += 1;
+            max = 1;
+          } else {
+            temp2 = temp;
+          }
+        } else {
+          temp2 = temp;
+          max += 1;
+        }
+      } else {
+        max = 1;
+        temp2 = [];
+      }
+      curr += 1;
+    }
+    setAnswer3(answer);
+  };
+
   // useEffect(() => {
   //   solution1();
   // });
@@ -56,7 +114,13 @@ const App: React.FC = () => {
       <header className="App-header">
         <h1>FiveJack</h1>
       </header>
-      <div>
+      <div
+        style={{
+          border: '3px solid black',
+          marginBottom: '50px',
+          padding: '20px',
+        }}
+      >
         <h1>Question 1</h1>
         <h3>Input</h3>
         <pre>{JSON.stringify(input1)}</pre>
@@ -64,6 +128,21 @@ const App: React.FC = () => {
         <button onClick={() => solution1(input1)}>Click to Answer</button>
         <br />
         <pre>{JSON.stringify(answer1)}</pre>
+      </div>
+      <div
+        style={{
+          border: '3px solid black',
+          marginBottom: '50px',
+          padding: '20px',
+        }}
+      >
+        <h1>Question 3</h1>
+        <h3>Input</h3>
+        <pre>{JSON.stringify(input3)}</pre>
+        <br />
+        <button onClick={() => solution3(input3)}>Click to Answer</button>
+        <br />
+        <pre>{JSON.stringify(answer3)}</pre>
       </div>
     </div>
   );
